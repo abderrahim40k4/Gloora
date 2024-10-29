ae9ce4f40d8222b7cf0c1ba4c5244c8672814bdc
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-10-29 17:17:08 +0100

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors:{
-      'baseColor':"#F3A17C",
-      'white':"#FFFFFF",
-      'black':"#000000",
-      'softGray':'#F8F7F7'
+    colors: {
+      'baseColor': "#F3A17C",
+      'white': "#FFFFFF",
+      'black': "#000000",
+      'softGray': '#F8F7F7'
     },
-  	extend: {
+    extend: {
       backgroundImage: theme => (
         {
-          'hero-pattern': "url('./src/assets/images/bg-big.png')",
-          'footer-texture': "url('./src/assets/images/bg-small.png')",
-          'footer-bg': "url('./src/assets/images/footer-bg.png')",
-          'about-bg': "url('./src/assets/images/imageHero.png')",
-          'product-bg': "url('./src/assetsimages/products-hero.png')",
-          'about-mid-bg': "url('./src/assets/images/bg-aboutmid.png')"
+          'hero-pattern': "url('/images/bg-big.png')",
+          'footer-texture': "url('/images/bg-small.png')",
+          'footer-bg': "url('/images/footer-bg.png')",
+          'about-bg': "url('/images/imageHero.png')",
+          'product-bg': "url('/images/products-hero.png')",
+          'about-mid-bg': "url('/images/bg-aboutmid.png')"
         }
       ),
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
       fontFamily: {
-        'Neue': ["PP Neue Montreal"," sans-serif"],
+        'Neue': ["PP Neue Montreal", " sans-serif"],
         'Averia': ["Averia Serif Libre", "serif"],
       },
-  		colors: {}
-  	}
+      colors: {}
+    }
   },
   plugins: [require("tailwindcss-animate")],
 }

17a81e5b367a7aa1caeb7164ed648a801d8978ec
abderrhim <132024050+abderrahim40k4@users.noreply.github.com>
2024-10-31 12:28:48 +0100

    responcive

--- src/components/shop/ProdactsList.tsx
+++ src/components/shop/ProdactsList.tsx
@@ -19,7 +19,7 @@ import Pagintion from "./Pagintion"
 
 const ProdactsList = () => {
   const [currentPage, setCurrentPage] = useState(1)
-  const [postePearPage] = useState(6)
+  const [postePearPage, setPostesPearPage] = useState(6)
  
 
   const lastPostIndex = currentPage * postePearPage;
@@ -29,7 +29,7 @@ const ProdactsList = () => {
   return (
     <div className="w-full">
       <div className='w-full md:px-12 px-3 flex md:flex-row flex-col md:gap-12 gap-8  mt-4'>
-        <div className='md:w-1/3 w-full py-12 md:h-full  md:px-8 px-4  flex flex-col gap-12 bg-softGray rounded-[50px]'>
+        <div className='md:w-1/3 w-full py-12 md:h-screen md:px-8 px-4  flex flex-col gap-12 bg-softGray rounded-[50px]'>
           <div className=''>
             <p className='font-Neue font-medium text-lg mb-6'>Product Categories</p>
             {
@@ -58,11 +58,11 @@ const ProdactsList = () => {
           {
             curentPost.map((product, index) => (
               <div className="w-full col-span-1 row-span-1" key={index}>
-                <div className="w-full h-[280px] bg-[#F7F7F7] rounded-3xl p-4 flex justify-between items-start flex-col">
+                <div className="w-full h-80 bg-[#F7F7F7] rounded-3xl p-4 flex justify-between items-start flex-col">
                   <div className="w-full">
                     <Badge variant={"default"} className="px-4 py-2 rounded-full bg-black text-white font-Neue text-sm font-light">-19%</Badge>
                   </div>
-                  <div className="w-full inline-flex items-center justify-center ">
+                  <div className="w-full inline-flex items-center justify-center">
                     <img src={ProdctIm} alt="" />
                   </div>
                   {/* condition actievted */}
